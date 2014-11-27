dac80b16ef48b7961be6968d05fa70b4c91f7313
Daniël W. Crompton <daniel.crompton@gmail.com>
2014-11-27 20:56:39 +0100

    spec version

--- spec/lib/podio_crm/version_spec.rb
+++ spec/lib/podio_crm/version_spec.rb
@@ -1,5 +1,10 @@
 require 'spec_helper'
 
 describe PodioCrm::Version do
-	it 'should have a test'
+	it 'version not empty' do
+		expect(described_class::STRING).to_not eq '..'
+	end
+	it 'version not nil' do
+		expect(described_class::STRING).to_not be_nil
+	end
 end
